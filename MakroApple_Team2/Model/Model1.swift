c44a88f4c03d30b1f470d2c8112c35925fb75d98
nathangun555 <nathangun555@gmail.com>
2025-10-06 17:25:03 +0700

    MVVM Model

---
+++ MakroApple_Team2/Model/Model1.swift
@@ -0,0 +1,7 @@
+//
+//  Model1.swift
+//  MakroApple_Team2
+//
+//  Created by Nathan Gunawan on 06/10/25.
+//
+
---
+++ MakroApple_Team2/ViewModel/ViewModel1.swift
@@ -0,0 +1,7 @@
+//
+//  ViewModel1.swift
+//  MakroApple_Team2
+//
+//  Created by Nathan Gunawan on 06/10/25.
+//
+
