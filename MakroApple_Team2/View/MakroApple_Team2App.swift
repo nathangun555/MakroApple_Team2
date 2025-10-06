cd724c6b24a2531ee50b3b84b7c5b33b605c76ca
nathangun555 <nathangun555@gmail.com>
2025-10-06 17:16:54 +0700

    Initial Xcode project setup

---
+++ MakroApple_Team2/View/MakroApple_Team2App.swift
@@ -0,0 +1,17 @@
+//
+//  MakroApple_Team2App.swift
+//  MakroApple_Team2
+//
+//  Created by Nathan Gunawan on 06/10/25.
+//
+
+import SwiftUI
+
+@main
+struct MakroApple_Team2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
