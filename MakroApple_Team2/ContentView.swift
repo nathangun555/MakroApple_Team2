1ddd2a2b58212851582da1575dd5429d2a1e2d98
Alfred <alfredwitono@gmail.com>
2025-10-13 17:07:31 +0700

    Feature: fix tabbar fix

---
+++ MakroApple_Team2/ContentView.swift
@@ -0,0 +1,39 @@
+//
+//  ContentView.swift
+//  MakroApple_Team2
+//
+//  Created by Nathan Gunawan on 06/10/25.
+//
+
+import SwiftUI
+import Foundation
+
+struct ContentView: View {
+  @State private var showNewOrder = false
+  @Environment(\.colorScheme) private var scheme
+  @Environment(\.accessibilityReduceTransparency) private var reduceTransparency
+
+  var body: some View {
+      TabView {
+          Tab("Orders", systemImage: "calendar") {
+              AllOrdersView()
+          }
+          Tab("Active", systemImage: "tray.full"){
+              ActiveOrdersView()
+          }
+          Tab("Analytics", systemImage: "chart.bar"){
+              AnalyticsView()
+          }
+          Tab("Settings", systemImage: "tray.full"){
+              SettingsView()
+          }
+          Tab("Add", systemImage: "plus.circle.fill", role: .search){
+              NewOrderView()
+          }
+      }
+  }
+}
+
+#Preview {
+    ContentView()
+}
