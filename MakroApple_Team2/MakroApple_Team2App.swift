3554a732ec481d8ac8e0ee6a719d9587fbe71b3a
nathangun555 <nathangun555@gmail.com>
2025-10-20 18:05:46 +0700

    sign in with apple backend

--- MakroApple_Team2/MakroApple_Team2App.swift
+++ MakroApple_Team2/MakroApple_Team2App.swift
@@ -9,9 +9,22 @@ import SwiftUI
 
 @main
 struct MakroApple_Team2App: App {
+    @StateObject private var session = SessionManager()
+
     var body: some Scene {
         WindowGroup {
             ContentView()
+                .environmentObject(session)   // ✅ Inject globally
         }
     }
 }
+
+
+//@main
+//struct MakroApple_Team2App: App {
+//    var body: some Scene {
+//        WindowGroup {
+//            SignInWithAppleView(isSignedIn: .constant(false), userId: .constant(nil))
+//        }
+//    }
+//}
--- MakroApple_Team2/View/ActiveOrdersView.swift
+++ MakroApple_Team2/View/ActiveOrdersView.swift
@@ -159,6 +159,4 @@ struct ActiveOrdersView: View {
     }
 }
 
-#Preview {
-    ActiveOrdersView()
-}
+
