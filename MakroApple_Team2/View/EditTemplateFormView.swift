95e5576a4e6b0835236180a59cf5c1a897e191de
Edward Suwandi <edwardsuwandi1402@gmail.com>
2025-10-23 15:11:40 +0700

    titip dulu

---
+++ MakroApple_Team2/View/EditTemplateFormView.swift
@@ -0,0 +1,15 @@
+//
+//  EditTemplateFormView.swift
+//  MakroApple_Team2
+//
+//  Created by Alfred Hans Witono on 22/10/25.
+//
+
+import SwiftUI
+import Foundation
+
+struct EditTemplateFormView: View {
+    var body: some View {
+        Text("EditTemplateFormView")
+    }
+}
