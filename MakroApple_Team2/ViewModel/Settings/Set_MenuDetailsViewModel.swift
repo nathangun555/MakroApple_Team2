35b4b865ee549267adc28394ef40b7e9833347b1
Alfred Hans <alfreddhanss@gmail.com>
2025-11-17 16:49:57 +0700

    feature: post usertesting 1, menu UI

--- MakroApple_Team2/ViewModel/Settings/Set_MenuDetailsViewModel.swift
+++ MakroApple_Team2/ViewModel/Settings/Set_MenuDetailsViewModel.swift
@@ -644,7 +644,8 @@ struct EditableProductRow: View {
             }
         }
         .padding(12)
-        .background(Color(.systemGray6))
+        .background(.deadlineCard)
+            .cornerRadius(10)
         .cornerRadius(14)
         .shadow(color: Color.black.opacity(0.05), radius: 3, x: 0, y: 1)
         .frame(maxWidth: .infinity)
