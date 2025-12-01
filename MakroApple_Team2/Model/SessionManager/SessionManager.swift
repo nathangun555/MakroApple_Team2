a0bec560336e737136d1ce5273a3f7c87ed31836
Alfred Hans <alfreddhanss@gmail.com>
2025-12-02 00:04:57 +0700

    SHOWCASE ID

--- MakroApple_Team2/Model/SessionManager/SessionManager.swift
+++ MakroApple_Team2/Model/SessionManager/SessionManager.swift
@@ -148,9 +148,11 @@ final class SessionManager: ObservableObject {
     // ✅ Dev mode setup - instant, synchronous
     private func setupDevSession() {
         self.isSignedIn = true
+        
+        self.userId = "dbbc99ea-1746-4673-afb1-a64754ed0312" // SHOWCASE
 //        self.userId = "49b3ba69-293d-496c-a52c-3e4f5b57329d" // new
 //        self.userId = "ec7eea39-e2b6-4e46-b847-646019276f67" // has template
-        self.userId = "083dc90d-ca03-4f45-a631-06fe21fe750f" // nathan analytics
+//        self.userId = "083dc90d-ca03-4f45-a631-06fe21fe750f" // nathan analytics
         
 //        self.userId = "49b3ba69-293d-496c-a52c-3e4f5b57329d" // Le.Aure
 //        self.userId = "bade0678-f878-4168-b605-c26e105b1936" // Petito
