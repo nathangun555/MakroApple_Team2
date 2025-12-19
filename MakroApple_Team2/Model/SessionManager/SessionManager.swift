4c4e26a1dbdadfadf208208e8855921b66e7258a
Edward Suwandi <148249720+iwakkk@users.noreply.github.com>
2025-12-20 00:00:03 +0700

    chore: merge for sprint 2

--- MakroApple_Team2/Model/SessionManager/SessionManager.swift
+++ MakroApple_Team2/Model/SessionManager/SessionManager.swift
@@ -150,7 +150,8 @@ final class SessionManager: ObservableObject {
         self.isSignedIn = true
         
         self.userId = "dbbc99ea-1746-4673-afb1-a64754ed0312" // SHOWCASE
-//        self.userId = "49b3ba69-293d-496c-a52c-3e4f5b57329d" // new
+        
+//        self.userId = "a4a00bc9-a7c7-47a6-8001-89c6fb8d760c" // new
 //        self.userId = "ec7eea39-e2b6-4e46-b847-646019276f67" // has template
 //        self.userId = "083dc90d-ca03-4f45-a631-06fe21fe750f" // nathan analytics
         
